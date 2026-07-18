c1a3ba8c1fa1d41b0fe76a40fe07a061b86db920
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-19 03:58:28 +0600

    Some practices for if-statement

--- Module-8.2/if.js
+++ Module-8.2/if.js
@@ -25,13 +25,21 @@ if (50 <= 55){
 }
 
 const price = 1400;
-
 if (price < 500){
     console.log('price is lower.');
 }
 
 const money = 5000;
-
 if (price < money){
     console.log('I will buy');
+}
+
+const name = 'Alu';
+if (name === 'Alu'){
+    console.log('We are Mr. Potato');
+}
+
+const isGorib = true;
+if (isGorib === true){
+    console.log('Yes, We are goribxx.');
 }
