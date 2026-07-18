5a6067205d6b6a9bf02b05a91dc347f4f03bfb1b
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-19 05:31:03 +0600

    Different ways to use multiple conditions

---
+++ Module-8.5/multiple.js
@@ -0,0 +1,32 @@
+/**
+ *      MULTIPLE CONDITIONS
+ *  
+ */
+
+const salary = 25000;
+const isBCS = false;
+const hasCar = true;
+
+if (salary > 50000 && isBCS === true && hasCar){
+    // console.log('Shu......Patro.');
+}
+else{
+    // console.log('Tor Kopale biya nai!!!!');
+}
+
+if (salary > 75000 || isBCS === true || hasCar){
+    console.log("Bolo baba kobul.");
+}
+else{
+    console.log("Vaag tui, moga.");
+}
+
+// ----------------------------------------------------
+
+// if ((salary > 50000 && hasCar) || isBCS) {
+
+// }
+
+// if (20 < 30 || (20 === 20 && 30 === 30)){
+    
+// }
