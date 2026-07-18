0ba36a5d81015d035e1ebc820e2a320f2b370958
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-19 03:52:29 +0600

    Conditionals in js

---
+++ Module-8.1/comparison.js
@@ -0,0 +1,17 @@
+/**
+ * 1. Greater Than: >
+ * 2. Less Than: <
+ * 3. equal to: ==, ===
+ * 4. Greater than or equal to: >=
+ * 5. Less than or equal to: <=
+ * 6. Not equal to: !=, !==
+ * 7. And: &&
+ * 8. Or: ||
+ */
+
+const a = 10;
+const b = 7;
+console.log(a > b);
+console.log(a < b);
+console.log(a === b);
+console.log(a !== b);
---
+++ Module-8.2/if.js
@@ -0,0 +1,37 @@
+/**
+ * Conditionals:
+ * If you give a treat, 
+ *   I will come.
+ * 
+ * -----------------------------------------------------------
+ *                      STRUCTURE
+ * -----------------------------------------------------------
+ * if(condition){
+ *  
+ * }
+ */
+
+if (true){
+    console.log('Inside the condition.'); 
+}
+
+if (8 > 3){
+    console.log('Inside the condition');
+    console.log('8 is bigger than 3.');
+}
+
+if (50 <= 55){
+    console.log('Coming inside the condition.');
+}
+
+const price = 1400;
+
+if (price < 500){
+    console.log('price is lower.');
+}
+
+const money = 5000;
+
+if (price < money){
+    console.log('I will buy');
+}
