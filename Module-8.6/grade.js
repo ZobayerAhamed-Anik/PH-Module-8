ff566613a3c0d9e52924f9ccb8aba147dfdefcd7
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-19 05:55:16 +0600

    Using if-else-if ladder solving a result grading problem.

---
+++ Module-8.6/grade.js
@@ -0,0 +1,36 @@
+//NSU grading systemic result...
+
+const marks = 79;
+if (marks >= 93){
+    console.log('A');
+}
+else if (marks >= 90){
+    console.log('A-');
+}
+else if (marks >= 87){
+    console.log('B+');
+}
+else if (marks >= 83){
+    console.log('B');
+}
+else if (marks >= 80){
+    console.log('B-');
+}
+else if (marks >= 77){
+    console.log('C+');
+}
+else if (marks >= 73){
+    console.log('C');
+}
+else if (marks >= 70){
+    console.log('C-');
+}
+else if (marks >= 67){
+    console.log('D+');
+}
+else if (marks >= 60){
+    console.log('D');
+}
+else {
+    console.log('F');
+}
