43bcd06f0cde10597bad97e100deece43e55837e
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-19 04:36:22 +0600

    Some practices for understanding else statement

--- Module-8.4/else.js
+++ Module-8.4/else.js
@@ -12,3 +12,22 @@
  * }
  */
  
+const rickshawFare = 200;
+if (rickshawFare < 50){
+    console.log('Mama hood tule den.');
+    console.log('GO SLOWLY, MAMA.');
+}
+else{
+    console.log('Walking is a good exercise.');
+    console.log('I am a situational runner.');
+}
+
+const x = 200;
+const y = 200;
+if (x !== y){
+    const z = x + y;
+    console.log('Value of Z:' , z);
+}
+else{
+    console.log('They are equal equal mukul mukul.')
+}
