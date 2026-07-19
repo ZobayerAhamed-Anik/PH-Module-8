c4d06cd10d40472d277d817931a81593a01815f9
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-20 05:08:22 +0600

    The idea of nested if-else

---
+++ Module-8.7/nested.js
@@ -0,0 +1,24 @@
+const money = 4000;
+
+if (money > 500){
+    console.log('Mama tui to borolok, amare khawa');
+    if (money > 1000){
+        console.log('Tui ki tutioner beton paisos?');
+        if (money > 5000){
+            console.log('Tui to amar apon bhai.');
+        }
+        else{
+            console.log('Not enough money. You have 1000 tk - 5000 tk');
+        }
+    }
+}
+else{
+    console.log('Amar kono borolok bondhu nai.');
+    const isHungry = false;
+    if (isHungry === true){
+
+    }
+    else{
+         
+    }
+}
