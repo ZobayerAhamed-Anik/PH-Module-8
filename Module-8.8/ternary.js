bfdbb70495c11ab2dc3cf5daa611cd67ff7e9325
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-23 07:46:44 +0600

    Ternary format if-else in practice.

---
+++ Module-8.8/ternary.js
@@ -0,0 +1,16 @@
+/**
+ * binary--> two;
+ * ternary--> three;
+ */
+
+let age = 17;
+if(age >= 18){
+    console.log('Please vote.');
+}
+else{
+    console.log('Grow up, Kid.');
+}
+
+age = 21;
+// format--> condition check ? if true: if false
+age >= 18 ? console.log('Please vote.') : console.log('Grow up, Kid.');
